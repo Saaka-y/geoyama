a4a7fafcd54dbf61a4455730ebedb318a9e41367
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-20 17:51:56 +0900

    👽️ fetch weather data depending on selected value by users

--- src/components/InfoPanel/InfoPanel.jsx
+++ src/components/InfoPanel/InfoPanel.jsx
@@ -3,20 +3,45 @@
 
 import { Filter } from "@/components/InfoPanel/Filter/Filter";
 import { ShowWeather } from "@/components/InfoPanel/Weather/ShowWeather";
+import { DateSelect } from "@/components/InfoPanel/Filter/DateSelect";
 
 export function InfoPanel({ mapState, mountainState, filterState, screenSizeState }) {
+
+  const {
+    distance,
+    setDistance,
+    courseTime,
+    setCourseTime,
+    selectedDate,
+    setSelectedDate, } = filterState;
+
   return (
-    <div className={`${screenSizeState.infoViewClass} p-6 md:p-8 mt-2 flex flex-col justify-start items-center md:justify-start gap-4 bg-(--color-surface)`}>
-      {!mapState.selectedMountain ? (
-        <Filter
-          mapState={mapState}
-          mountainState={mountainState}
-          filterState={filterState}
-          screenSizeState={screenSizeState}
+    <>
+      <div className={`${screenSizeState.infoViewClass} p-6 md:p-8 mt-2 flex flex-col justify-start items-center md:justify-start gap-4 bg-(--color-surface)`}>
+        {!mapState.selectedMountain ? (
+
+          <Filter
+            mapState={mapState}
+            mountainState={mountainState}
+            filterState={filterState}
+            screenSizeState={screenSizeState}
+          />
+        ) : (
+          <ShowWeather
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            selectedMountain={mapState.selectedMountain}
+          />
+        )}
+        <div className=" w-[90%] flex flex-row md:flex-col items-center md:items-start gap-2">
+        <p className="w-1/2 md:w-full text-left text-xs">Select Date:</p>
+        <DateSelect
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          className="w-2/3 md:w-full border border-gray-400 rounded px-2 py-1"
         />
-      ) : (
-        <ShowWeather />
-      )}
-    </div>
+      </div>
+      </div>
+    </>
   );
 }
--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -1,7 +1,8 @@
 
 export default async function handler(req, res) {
+  const { lat, lon } = req.query;
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=35.6513&lon=139&units=metric&appid=${process.env.OPENWEATHER_KEY}`
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.OPENWEATHER_KEY}`
   );
   const data = await response.json();
   res.status(200).json(data);
