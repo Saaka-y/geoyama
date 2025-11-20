88c4497026bf763fef1de5d250059c5ab548f5d7
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-20 18:15:24 +0900

    💄 move DateSelect component to InfoPanel to show it on both Filter and ShowWeather components

--- src/components/InfoPanel/InfoPanel.jsx
+++ src/components/InfoPanel/InfoPanel.jsx
@@ -5,7 +5,7 @@ import { Filter } from "@/components/InfoPanel/Filter/Filter";
 import { ShowWeather } from "@/components/InfoPanel/Weather/ShowWeather";
 import { DateSelect } from "@/components/InfoPanel/Filter/DateSelect";
 
-export function InfoPanel({ mapState, mountainState, filterState, screenSizeState }) {
+export function InfoPanel({ mapState, mountainState, filterState }) {
 
   const {
     distance,
@@ -17,30 +17,26 @@ export function InfoPanel({ mapState, mountainState, filterState, screenSizeStat
 
   return (
     <>
-      <div className={`${screenSizeState.infoViewClass} p-6 md:p-8 mt-2 flex flex-col justify-start items-center md:justify-start gap-4 bg-(--color-surface)`}>
-        {!mapState.selectedMountain ? (
-
-          <Filter
-            mapState={mapState}
-            mountainState={mountainState}
-            filterState={filterState}
-            screenSizeState={screenSizeState}
-          />
-        ) : (
-          <ShowWeather
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-            selectedMountain={mapState.selectedMountain}
-          />
-        )}
-        <div className=" w-[90%] flex flex-row md:flex-col items-center md:items-start gap-2">
+      {!mapState.selectedMountain ? (
+        <Filter
+          mapState={mapState}
+          mountainState={mountainState}
+          filterState={filterState}
+        />
+      ) : (
+        <ShowWeather
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+          selectedMountain={mapState.selectedMountain}
+        />
+      )}
+      <div className=" w-[90%] flex flex-row md:flex-col items-center md:items-start gap-2">
         <p className="w-1/2 md:w-full text-left text-xs">Select Date:</p>
         <DateSelect
           selectedDate={selectedDate}
           setSelectedDate={setSelectedDate}
           className="w-2/3 md:w-full border border-gray-400 rounded px-2 py-1"
         />
-      </div>
       </div>
     </>
   );
