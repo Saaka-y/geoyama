642a17beef9c80a230057204d1081612da1cd28a
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-24 13:48:21 +0900

    💄 slightly update weather card UI

--- src/components/InfoPanel/Weather/ShowWeather.jsx
+++ src/components/InfoPanel/Weather/ShowWeather.jsx
@@ -7,6 +7,7 @@ import { useEffect, useState, useRef } from "react";
 export function ShowWeather({ selectedMountain, selectedDate, setSelectedDate }) {
   const [forecast, setForecast] = useState([]);
   const scrollRef = useRef(null);
+  const elevation = selectedMountain.properties.summit
 
   //**************************/
   // fetch /
@@ -51,7 +52,7 @@ export function ShowWeather({ selectedMountain, selectedDate, setSelectedDate })
   // wind direction /
   //**************************/
   function degToCardinal(deg) {
-    const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    const directions = ['↓', '↙︎', '←', '↖︎', '↑', '↗︎', '→', '↘︎'];
     const index = Math.round(deg / 45) % 8;
     return directions[index];
   }
@@ -82,51 +83,59 @@ export function ShowWeather({ selectedMountain, selectedDate, setSelectedDate })
 
   if (!selectedMountain || forecast.length === 0) return <p>Loading weather...</p>;
 
-  return (
-<div
-  ref={scrollRef}
-  className="flex md:flex-col gap-4 overflow-x-auto md:overflow-x-hidden md:overflow-y-auto snap-x snap-mandatory px-2 md:px-4"
->
-  {Object.keys(grouped).map((date) => (
-    <div
-      key={date}
-      id={`date-${date}`}
-      className="snap-start bg-white border-2 rounded-lg p-2 flex flex-col gap-2"
-      style={{
-        backgroundColor: date === selectedDate.value ? "cadetblue" : "#BDB76B",
-        borderColor: date === selectedDate.value ? "black" : "gray"
-      }}
-    >
-      {/* 日付ヘッダー */}
-      <p className="text-sm font-bold mb-2 text-center">{date}</p>
+  // ****** parent div ******
+  // flex-1 bg-(--color-surface)` +
+  //   (showWeather? ` pt-6` ...)　
 
-      {/* カード群 */}
-      <div className="flex flex-row md:flex-col gap-2">
-        {grouped[date].map((item, i) => {
-          const time = item.dt_txt.slice(11, 16);
-          return (
-            <div
-              key={i}
-              className="w-[120px] md:w-full border rounded p-2 text-center bg-gray-300 flex-shrink-0"
-            >
-              <p className="text-xs mb-1">{time}</p>
-              <img
-                src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                alt={item.weather[0].description}
-                className="mx-auto"
-              />
-              <p className="text-sm font-bold">{item.main.temp}°C</p>
-              <p className="text-sm font-bold">
-                {degToCardinal(item.wind.deg)} {item.wind.speed} m/s
-              </p>
-              <p className="text-xs">{item.weather[0].description}</p>
-            </div>
-          );
-        })}
-      </div>
+  
+  return (
+    <div
+      ref={scrollRef}
+      className="flex md:flex-col gap-4 overflow-x-auto md:overflow-x-hidden md:overflow-y-auto px-4 pb-4 md:px-4"
+    > 
+    {/* 天気スクロールカードと画面端の距離を変える場合、親ではなく ↑ のpaddingを変える。親のMainView divを変えるとカードが見切れてしまう */}
+      {Object.keys(grouped).map((date) => (
+        <div
+          key={date}
+          id={`date-${date}`}
+          className="snap-start bg-white p-2 flex flex-col gap-1"
+          style={{
+            border: date === selectedDate.value
+              ? "2px solid #3399FF"
+              : "1px solid black"
+          }}
+        >
+          {/* 日付ヘッダー */}
+          <p className="text-sm font-bold mb-2 text-center">{date} <span className="text-gray-400"> - Temp: around the summit ({elevation}m) </span></p>
+
+          {/* カード群 */}
+          <div className="flex flex-row md:flex-col gap-2">
+            {grouped[date].map((item, i) => {
+              const time = item.dt_txt.slice(11, 16);
+              const temp = Math.round(item.wind.speed);
+              return (
+                <div
+                  key={i}
+                  className="w-[120px] md:w-full border rounded p-2 text-center bg-gray-300 shrink-0"
+                >
+                  <p className="text-xs mb-1">{time}</p>
+                  <img
+                    src={`https://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                    alt={item.weather[0].description}
+                    className="mx-auto"
+                  />
+                  <p className={`text-sm font-bold ${temp <= 0 ? "text-blue-500" : ""}`}>{temp}°C</p>
+                  <p className="text-sm font-bold">
+                    {degToCardinal(item.wind.deg)} {Math.round(item.wind.speed)} m/s
+                  </p>
+                  <p className="text-xs">{item.weather[0].description}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
 
   );
 }
