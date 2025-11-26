a202deb2c761b17a173c7a46d4bec4ab5267f214
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-26 11:19:40 +0900

    ✨add Nantai-san info

---
+++ src/pages/api/nantaiGeoJson.js
@@ -0,0 +1,26 @@
+// pages/api/nantaiGeoJson.js
+import fs from 'fs';
+import path from 'path';
+import { DOMParser } from '@xmldom/xmldom';
+import { gpx } from '@mapbox/togeojson';
+
+export default function NabewariGeoJson(req, res) {
+  try {
+    // GPXファイルパス
+    const gpxFilePath = path.join(process.cwd(), 'public', 'Nantai.gpx');
+
+    // ファイル読み込み
+    const gpxData = fs.readFileSync(gpxFilePath, 'utf-8');
+
+    // DOM に変換
+    const dom = new DOMParser().parseFromString(gpxData, 'text/xml');
+
+    // GeoJSON に変換
+    const geojson = gpx(dom);
+
+    res.status(200).json({ geojson });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to convert GPX to GeoJSON' });
+  }
+}
