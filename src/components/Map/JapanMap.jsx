356964ae531857ccff700119121c2a895e3f9ce3
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-11-30 17:19:50 +0900

    💄 remove map labels

--- src/components/Map/JapanMap.jsx
+++ src/components/Map/JapanMap.jsx
@@ -20,26 +20,27 @@ export function JapanMap({ japanMapRef, filteredMountains, initialView, selected
       container: japanMapContainerRef.current, // container ID
       style: 'mapbox://styles/saaka/cmigzixmw00d701r9d48afpqe', // style URL
     });
-  }, [])
 
-  // Marker 作成、フィルターに沿って変更
-  useEffect(() => {
-    if (!japanMapRef.current) return;
+    }, [])
 
-    // 既存のマーカーを消す
-    markerRef.current.forEach(m => m.remove());
-    markerRef.current = [];
+    // Marker 作成、フィルターに沿って変更
+    useEffect(() => {
+      if (!japanMapRef.current) return;
 
-    filteredMountains?.forEach((m) => {
-      const coords = m.geometry.coordinates;
+      // 既存のマーカーを消す
+      markerRef.current.forEach(m => m.remove());
+      markerRef.current = [];
 
-      // Popup用のDOM作成
-      const popupEl = document.createElement("div");
-      popupEl.className = "p-1 bg-gray-300/40 text-black rounded shadow-md";
+      filteredMountains?.forEach((m) => {
+        const coords = m.geometry.coordinates;
 
-      // 山情報
-      const info = document.createElement("div");
-      info.innerHTML = `
+        // Popup用のDOM作成
+        const popupEl = document.createElement("div");
+        popupEl.className = "p-1 bg-gray-300/40 text-black rounded shadow-md";
+
+        // 山情報
+        const info = document.createElement("div");
+        info.innerHTML = `
         <span class="font-bold">${m.properties.title} (${m.properties.summit}m)</span>
         <span class="italic">${m.properties.routeName ? `- ${m.properties.routeName}` : ""}</span>
         <br/>
@@ -49,50 +50,50 @@ export function JapanMap({ japanMapRef, filteredMountains, initialView, selected
         <br/>
         Elevation gain: ${m.properties.elevation}m
       `;
-      popupEl.appendChild(info);
+        popupEl.appendChild(info);
 
-      // ボタン
-      const button = document.createElement("button");
-      button.className = "underline cursor-pointer mt-1";
-      button.textContent = "Weather and trail map";
-      button.addEventListener("click", () => {
-        setSelectedMountain(m);
-        setShowFocusMap(true)
-      });
-      popupEl.appendChild(button);
+        // ボタン
+        const button = document.createElement("button");
+        button.className = "underline cursor-pointer mt-1";
+        button.textContent = "Weather and trail map";
+        button.addEventListener("click", () => {
+          setSelectedMountain(m);
+          setShowFocusMap(true)
+        });
+        popupEl.appendChild(button);
 
-      // Popup
-      const popup = new mapboxgl.Popup({ offset: 25, anchor: "top" })
-        .setDOMContent(popupEl)
-        
+        // Popup
+        const popup = new mapboxgl.Popup({ offset: 25, anchor: "top" })
+          .setDOMContent(popupEl)
 
-      // Marker作成、Popupを紐付ける
-      const marker = new mapboxgl.Marker()
-        .setLngLat(coords)
-        .setPopup(popup)
-        .addTo(japanMapRef.current);
 
-      marker.getElement().addEventListener("click", () => {
-        japanMapRef.current.flyTo({
-          ...initialView,
-          center: coords,
+        // Marker作成、Popupを紐付ける
+        const marker = new mapboxgl.Marker()
+          .setLngLat(coords)
+          .setPopup(popup)
+          .addTo(japanMapRef.current);
+
+        marker.getElement().addEventListener("click", () => {
+          japanMapRef.current.flyTo({
+            ...initialView,
+            center: coords,
+          })
         })
+
+        // Marker 管理用配列に追加
+        markerRef.current.push(marker);
       })
-
-      // Marker 管理用配列に追加
-      markerRef.current.push(marker);
-    })
-  }, [filteredMountains]);
+    }, [filteredMountains]);
 
 
-  return (
-    <div
-      style={{ height: '100%', width: "100%" }}
-      ref={japanMapContainerRef}
-      className="map-container"
-    />
+    return (
+      <div
+        style={{ height: '100%', width: "100%" }}
+        ref={japanMapContainerRef}
+        className="map-container"
+      />
 
-  );
-}
+    );
+  }
 
 
