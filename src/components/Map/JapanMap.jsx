45c464d68ebd46cc82d78102ec5d9ed4aec6edd9
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-15 17:13:40 +0900

    ♻️ Divide FocusMap component into hooks and add an isMapReady state to handle map loading issues

--- src/components/Map/JapanMap.jsx
+++ src/components/Map/JapanMap.jsx
@@ -2,7 +2,7 @@
 "use client"
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useMapUiStore } from '@/stores/mapUiStore';
 import { useIsLandscape } from '@/hooks/useIsLandscape';
 import { useCreateMarker } from '@/hooks/useCreateMarker';
@@ -10,10 +10,12 @@ import { useCreateMarker } from '@/hooks/useCreateMarker';
 const accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
 export function JapanMap({ japanMapRef }) {
+  const [isMapReady, setIsMapReady] = useState(false); // a flag that shows if map is ready
+
   const { japanMapInitialView } = useMapUiStore();
 
   const isLandscape = useIsLandscape();
-  const markerRef = useCreateMarker(japanMapRef);
+  useCreateMarker({ japanMapRef, ready: isMapReady });
 
   const japanMapContainerRef = useRef(null);
 
@@ -26,15 +28,21 @@ export function JapanMap({ japanMapRef }) {
       container: japanMapContainerRef.current,
       style: 'mapbox://styles/mapbox/outdoors-v12',
     });
-  }, [])
+
+    japanMapRef.current.on('load', () => {
+      setIsMapReady(true); // a flag that shows if map is ready
+    });
+
+    return () => japanMapRef.current?.remove();
+  }, [japanMapInitialView, japanMapRef]);
 
   // Mapのリサイズ
   useEffect(() => {
     if (!japanMapRef.current) return;
     japanMapRef.current.resize();
-  }, [isLandscape])
-  
-  
+  }, [isLandscape, japanMapRef])
+
+
   return (
     <div
       style={{ height: '100%', width: "100%" }}
