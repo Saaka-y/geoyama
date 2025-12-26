245320050a96a73e36ccbf622e19fac01755e62c
Sara <188415334+Saaka-y@users.noreply.github.com>
2025-12-26 14:21:55 +0900

    🐛 fix back button to avoid "japanMapRef undefined" error

--- src/components/MainView/BackButton.jsx
+++ src/components/MainView/BackButton.jsx
@@ -1,26 +1,32 @@
 // components/MainView/BackButton.jsx
 "use client";
 import { useMapUiStore } from "@/stores/mapUiStore";
+import { useEffect } from "react";
 
 export function BackButton({ japanMapRef }) {
   const { showFocusMap, backToMap, japanMapInitialView } = useMapUiStore();
 
+  useEffect(() => {
+    if (!showFocusMap && japanMapRef.current) {
+      japanMapRef?.current?.flyTo({
+        ...japanMapInitialView,
+      });
+    }
+  }, [showFocusMap, japanMapInitialView, japanMapRef])
+
   const handleBackToMap = () => {
     backToMap();
-    japanMapRef?.current.flyTo({
-      ...japanMapInitialView,
-    });
   };
 
   const className = [
-  "bg-white/90 backdrop-blur-sm",
-  "border border-gray-300 shadow-md",
-  "flex items-center justify-center gap-1",
-  "px-4 py-1",
-  "w-full md:w-24 landscape:w-24",
-  "text-sm font-semibold",
-  "hover:bg-white hover:shadow-lg cursor-pointer",
-].join(" ");
+    "bg-white/90 backdrop-blur-sm",
+    "border border-gray-300 shadow-md",
+    "flex items-center justify-center gap-1",
+    "px-4 py-1",
+    "w-full md:w-24 landscape:w-24",
+    "text-sm font-semibold",
+    "hover:bg-white hover:shadow-lg cursor-pointer",
+  ].join(" ");
 
   return (
     <>
